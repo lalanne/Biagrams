e9159625bad9cd50963ae61ff403d152695305d8
Christian <lalanne@gmail.com>
2014-04-28 20:30:54 +0100

    taking TokensIterator outside of Tokens

--- Tokens.hpp
+++ Tokens.hpp
@@ -3,41 +3,41 @@
 #include <string>
 #include <sstream>
 
+class TokensIterator{
+    public:
+        TokensIterator(std::vector<std::string>::iterator it) : it(it){}
+        TokensIterator& operator++(){
+            ++it;
+            return *this;
+        }
+
+        TokensIterator operator++(int){
+            TokensIterator tmp(*this);
+            operator++();
+            return tmp;
+        }
+
+        bool operator>(const TokensIterator& rhs){
+            return it>rhs.it;
+        }
+
+        bool operator<( const TokensIterator& rhs){
+            return it<rhs.it;
+        }
+
+        std::string& operator*(){
+            return *it;
+        }
+
+        TokensIterator operator+(const unsigned int i){
+            return TokensIterator(it+i);
+        }
+
+    private:
+        std::vector<std::string>::iterator it; 
+};
+
 class Tokens{
-    class TokensIterator{
-        public:
-            TokensIterator(std::vector<std::string>::iterator it) : it(it){}
-            TokensIterator& operator++(){
-                ++it;
-                return *this;
-            }
-
-            TokensIterator operator++(int){
-                TokensIterator tmp(*this);
-                operator++();
-                return tmp;
-            }
-
-            bool operator>(const TokensIterator& rhs){
-                return it>rhs.it;
-            }
-
-            bool operator<( const TokensIterator& rhs){
-                return it<rhs.it;
-            }
-
-            std::string& operator*(){
-                return *it;
-            }
-
-            TokensIterator operator+(const unsigned int i){
-                return TokensIterator(it+i);
-            }
-
-        private:
-            std::vector<std::string>::iterator it; 
-    };
-
     public:
         Tokens(std::istringstream& iss):tokens{std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{}}{}
 
@@ -51,6 +51,7 @@ class Tokens{
 
     private:
         std::vector<std::string> tokens;
-
 };
+
+
 
